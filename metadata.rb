982a353f8824e48881a4f916fd00300d309bb6d7
Tim Smith <tsmith84@gmail.com>
2015-09-24 21:40:35 -0700

    Add long_description and supports 'windows'
    Adding the supports improves search in Supermarket

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer 'Jeff Blaine'
 maintainer_email 'jblaine@kickflop.net'
 license 'mit'
 description 'Installs/Configures Sysmon from the Sysinternals suite'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 long_description 'Installs/Configures from the Sysinternals suite'
 version '0.1.0'
+supports 'windows'
 
 depends 'windows'
